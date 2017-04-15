2064f2c1d0e89d2104037a84239fd1806a948e40
gramosubuntu <gerard.ramosm89@gmail.com>
2017-04-14 17:22:39 -0700

    changes

--- test/create_test.js
+++ test/create_test.js
@@ -1,7 +1,13 @@
 const assert = require('assert');
+const User = require('../src/user');
 
 describe('Creating records', () => {
-  it('saves a user', () => {
+  it('saves a user', (done) => {
+    const gerry = new User({ name: 'Gerry' });
+    gerry.save().then(() => {
+      assert(!gerry.isNew);
+      done();
+    });
 
   });
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/users_test');
-mongoose.connection
-  .once('open', () => console.log('Good to go!'))
-  .on('error', (error) => {
-    console.warn('Warning', error);
+before((done) => {
+  mongoose.connect('mongodb://localhost:27017/users_test');
+  mongoose.connection
+    .once('open', () => {
+      console.log('Good to go!');
+      done();
+    })
+    .on('error', (error) => {
+      console.warn('Warning', error);
+    });
+});
+
+
+beforeEach((done) => {
+  console.log('hit before each, dropping db');
+  mongoose.connection.collections.users.drop(() => {
+    // Ready to run the next test
+    done();
   });
+});
