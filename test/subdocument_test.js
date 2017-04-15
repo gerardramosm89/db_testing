8aaa5725084d6a2dcf4c5070fb0b47d1e92d9bfa
Cudablogv2 <gerard.ramosm89@gmail.com>
2017-04-15 10:53:23 -0700

    changes

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -16,4 +16,44 @@ describe('Subdocuments', () => {
         done();
       });
   });
+
+  it('Can add subdocuments to an existing record', (done) => {
+    const gerry = new User({ name: 'Gerry', posts: []});
+    gerry.save()
+      .then(() => {
+        return User.findOne({ name: 'Gerry' })
+      })
+      .then((user => {
+        user.posts.push({ title: 'New Post' });
+        return user.save();
+      }))
+      .then(() => {
+        return User.findOne({ name: 'Gerry' });
+      })
+      .then((user) => {
+        assert(user.posts[0].title == 'New Post');
+        done();
+      });
+  });
+
+  it('Can remove an existing subdocument', (done) => {
+    const gerry = new User({ 
+      name: 'Gerry',
+      posts: [{ title: 'New Title' }]
+    });
+
+    gerry.save()
+      .then(() => {
+        return User.findOne({ name: 'Gerry' })
+      })
+      .then((user) => {
+        user.posts[0].remove(); // When calling .remove() for a subdocument you still have to save class again
+        return user.save();
+      })
+      .then((user) => {
+        assert(user.posts.length == 0);
+        done();
+      });
+  });
+
 });
---
+++ test/virtual_type_test.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Virtual types', () => {
+  it('postCount returns number of posts', (done) => {
+    const gerry = new User({
+      name: 'Gerry',
+      posts: [{ title: 'PostTitle'}]
+    });
+
+    gerry.save()
+      .then(() => User.findOne({ name: 'Gerry' }))
+      .then((user) => {
+        console.log(user);
+        gerry.set('postCount', 1);
+        gerry.save();
+        assert(gerry.postCount == 1);
+        done();
+      });
+  });
+});
