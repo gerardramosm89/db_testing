161d2656e5bc20dbec26cc9d4f347ce1376b2cd3
gramosubuntu <gerard.ramosm89@gmail.com>
2017-04-18 09:12:10 -0700

    changes

--- react-redux/index.js
+++ react-redux/index.js
@@ -1,28 +1,28 @@
-      var store = Redux.createStore(combineReducer)
+var store = Redux.createStore(combineReducer)
 
-      function render() {
-         var state = store.getState();
-         document.getElementById('value').innerHTML = state.count;
-         document.getElementById('value2').innerHTML = state.sum;
-      };
-      store.subscribe(render);
+function render() {
+    var state = store.getState();
+    document.getElementById('value').innerHTML = state.count;
+    document.getElementById('value2').innerHTML = state.sum;
+};
+store.subscribe(render);
 
-      render();
+render();
 
 
 
-      //~end step 2.2
-      document.getElementById('decrement')
-        .addEventListener('click', function () {
-          store.dispatch(decrease());
-        })
-      document.getElementById('incrementAsync')
-        .addEventListener('click', function () {
-              store.dispatch(increase());
-        })
-      document.getElementById('sum')
-        .addEventListener('click', function () {
-          var a = document.getElementById('a').value;
-          var b = document.getElementById('b').value;
-          store.dispatch(getSum(a, b));
-        })
+//~end step 2.2
+document.getElementById('decrement')
+  .addEventListener('click', function () {
+    store.dispatch(decrease());
+  })
+document.getElementById('incrementAsync')
+  .addEventListener('click', function () {
+        store.dispatch(increase());
+  })
+document.getElementById('sum')
+  .addEventListener('click', function () {
+    var a = document.getElementById('a').value;
+    var b = document.getElementById('b').value;
+    store.dispatch(getSum(a, b));
+  })
