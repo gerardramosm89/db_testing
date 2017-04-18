f86f4705bf6cfb02bf39304a11c8addce0c31cfe
Cudablogv2 <gerard.ramosm89@gmail.com>
2017-04-17 21:05:00 -0700

    changes

--- react-redux/index.js
+++ react-redux/index.js
@@ -1,18 +1,15 @@
-      // step 1.2
       var store = Redux.createStore(combineReducer)
-      // ~end step 1.2
-      // step 1.3
+
       function render() {
          var state = store.getState();
          document.getElementById('value').innerHTML = state.count;
-         document.querySelector('#value2').innerHTML = state.sum;
-        //  document.getElementById('value2').innerHTML = state.a + state.b
+         document.getElementById('value2').innerHTML = state.sum;
       };
       store.subscribe(render);
-      // ~end step 1.3
+
       render();
 
-      // step 2.2
+
 
       //~end step 2.2
       document.getElementById('decrement')
@@ -21,19 +18,11 @@
         })
       document.getElementById('incrementAsync')
         .addEventListener('click', function () {
-          setTimeout(
-            function increment () {
-
               store.dispatch(increase());
-
-            }
-          , 1000);
         })
       document.getElementById('sum')
         .addEventListener('click', function () {
           var a = document.getElementById('a').value;
           var b = document.getElementById('b').value;
-          a = parseInt(a);
-          b = parseInt(b);
           store.dispatch(getSum(a, b));
         })
--- react-redux/reducers/index.js
+++ react-redux/reducers/index.js
@@ -1,26 +1,15 @@
-function counter(currentState, action) {
-  DEFAULT_STATE = 0;
-  var nextState = '?';
-  return nextState
-}
-
-function sum(currentState, action) {
-  DEFAULT_STATE = 77;
-  var nextState = '???';
-  return nextState
-}
-
 function combineReducer(currentState, action) {
-  var DEFAULT_STATE = {count:0, a: 0, b: 0, sum: 0};
-  var nextState = Object.assign({}, currentState);
+  // var DEFAULT_STATE = {count:0, sum: 0};
+  var nextState = Object.assign({},currentState);
+
   nextState = {
     count: counter(nextState.count, action),
     sum: sum(nextState.sum, action)
-  }
+  };
   return nextState;
   // if (currentState === undefined) { // look at to Note 1.1
 
-  //   nextState = DEFAULT_STATE;// Note1.2
+  //   nextState = DEFAULT_STATE;
   //   return nextState;
   // }
   // switch (action.type) {
@@ -31,9 +20,8 @@ function combineReducer(currentState, action) {
   //     nextState.count = currentState.count + 1;
   //     return nextState;// Note2.2
   //   case 'SUM':
-  //     nextState.a = action.a;
-  //     nextState.b = action.b;
-  //     nextState.sum = parseInt(action.a) + parseInt(action.b);
+  //     var sum = parseInt(action.a) + parseInt(action.b);
+  //     nextState.sum = sum;
   //     return nextState;
   //   default:
   //     return nextState;
